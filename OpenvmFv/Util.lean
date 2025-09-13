07ed64cd6304ea06866578c0783ec05e8858bf4b
Dom Henderson <dom@nethermind.io>
2025-09-13 13:24:36 +0100

    WIP mul

--- OpenvmFv/Util.lean
+++ OpenvmFv/Util.lean
@@ -15,34 +15,41 @@ section BusIndices
   abbrev RangeCheckerBus : ℕ := 4
   abbrev ReadInstructionBus : ℕ := 8
   abbrev BitwiseBus : ℕ := 9
+  abbrev RangeTupleCheckerBus : ℕ := 11
 
   @[openvm_encapsulation]
-  lemma Rv32BaseAluAdapterAir_execution_bus:
+  lemma execution_bus_simplifcation:
     (if index = 0 then a else b) =
     if index = ExecutionBus then a else b
   := rfl
 
   @[openvm_encapsulation]
-  lemma Rv32BaseAluAdapterAir_memory_bus:
+  lemma memory_bus_simplifcation:
     (if index = 1 then a else b) =
     if index = MemoryBus then a else b
   := rfl
 
   @[openvm_encapsulation]
-  lemma Rv32BaseAluAdapterAir_range_checker_bus:
+  lemma range_checker_bus_simplifcation:
     (if index = 4 then a else b) =
     if index = RangeCheckerBus then a else b
   := rfl
 
   @[openvm_encapsulation]
-  lemma Rv32BaseAluAdapterAir_read_instruction_bus:
+  lemma read_instruction_bus_simplifcation:
     (if index = 8 then a else b) =
     if index = ReadInstructionBus then a else b
   := rfl
 
   @[openvm_encapsulation]
-  lemma Rv32BaseAluAdapterAir_bitwise_bus:
+  lemma bitwise_bus_simplifcation:
     (if index = 9 then a else b) =
     if index = BitwiseBus then a else b
+  := rfl
+
+  @[openvm_encapsulation]
+  lemma range_tuple_checker_bus_simplifcation:
+    (if index = 11 then a else b) =
+    if index = RangeTupleCheckerBus then a else b
   := rfl
 end BusIndices
